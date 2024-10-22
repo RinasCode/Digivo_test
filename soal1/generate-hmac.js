f1c865ec700856e7c9677a5480f91f2f09d278a7
Rina Sismita <rinasismita@gmail.com>
2024-10-22 15:01:04 +0700

    soal 1 done

---
+++ soal1/generate-hmac.js
@@ -0,0 +1,11 @@
+const crypto = require('crypto');
+
+const body = JSON.stringify({
+  url_gambar: "https://res.cloudinary.com/dcisb7ayn/image/upload/v1725076891/u5ub4flw4jsqk0efcpf1.jpg",
+  persentase_kompresi: 160
+});
+
+const HMAC_SECRET = 'your-secret-key'; 
+const hash = crypto.createHmac('sha512', HMAC_SECRET).update(body).digest('hex');
+
+console.log(`HMAC Hash: ${hash}`); 
